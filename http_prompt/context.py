8d511f5297c805f8c4412f74fb498d93f0da4a7a
Chang-Hung Liang <eliang.cs@gmail.com>
2016-04-27 17:08:07 +0800

    Implement command execution visitor

--- http_prompt/context.py
+++ http_prompt/context.py
@@ -1,14 +1,37 @@
 class Context(object):
 
-    def __init__(self, url):
+    def __init__(self, url=None):
         self.url = url
         self.headers = {}
         self.querystring_params = {}
         self.body_params = {}
         self.options = []
 
-    def to_args(self):
-        args = [self.url] + self.options
+    def copy(self):
+        context = Context(self.url)
+        context.headers = self.headers.copy()
+        context.querystring_params = self.querystring_params.copy()
+        context.body_params = self.body_params.copy()
+        context.options = list(self.options)
+        return context
+
+    def update(self, context):
+        if context.url:
+            self.url = context.url
+
+        self.headers.update(context.headers)
+        self.querystring_params.update(context.querystring_params)
+        self.body_params.update(context.body_params)
+
+        for opt in context.options:
+            if opt not in self.options:
+                self.options.append(opt)
+
+    def httpie_args(self, method=None):
+        args = list(self.options)
+        if method:
+            args.append(method.upper())
+        args.append(self.url)
         for k, v in self.headers.iteritems():
             args.append('%s:%s' % (k, v))
         for k, v in self.querystring_params.iteritems():
@@ -16,3 +39,6 @@ class Context(object):
         for k, v in self.body_params.iteritems():
             args.append('%s=%s' % (k, v))
         return args
+
+    def curl_args(self):
+        raise NotImplementedError('curl is not supported yet')
--- http_prompt/execution.py
+++ http_prompt/execution.py
@@ -1,8 +1,13 @@
+import click
+
 from urlparse import urljoin
 
 from httpie.core import main as httpie_main
+from parsimonious.exceptions import ParseError
 from parsimonious.grammar import Grammar
 from parsimonious.nodes import NodeVisitor
+
+from .context import Context
 
 
 grammar = Grammar(r"""
@@ -13,16 +18,15 @@ grammar = Grammar(r"""
 
     concat_mutation = header_mutation / querystring_mutation / body_mutation /
                       option_mutation
-    nonconcat_mutation = cd / su / "reset"
-    preview = _ tool _ action
+    nonconcat_mutation = cd / "reset"
+    preview = _ tool _ (method _)? concat_mutation*
     action = _ method _ concat_mutation*
 
     header_mutation = _ varname ":" string _
     querystring_mutation = _ varname "==" string _
-    body_mutation = _ string "=" string _
+    body_mutation = _ varname "=" string _
     option_mutation = _ "--" varname _
     cd = _ "cd" _ string _
-    su = _ "su" _ string _
     tool = "httpie" / "curl"
     method = "get" / "post" / "put" / "delete" / "patch"
 
@@ -32,34 +36,94 @@ grammar = Grammar(r"""
 """)
 
 
+def urljoin2(base, path, **kwargs):
+    if not base.endswith('/'):
+        base += '/'
+    url = urljoin(base, path)
+    if url.endswith('/'):
+        url = url[:-1]
+    return url
+
+
 class ExecutionVisitor(NodeVisitor):
 
     def __init__(self, context):
         super(ExecutionVisitor, self).__init__()
         self.context = context
 
+        self.context_override = Context(context.url)
+        self.method = None
+        self.tool = None
+
     def visit_method(self, node, children):
-        httpie_main([node.text] + self.context.to_args())
+        self.method = node.text
         return node
 
     def visit_cd(self, node, children):
         path = children[3].text
-        self.context.url = urljoin(self.context.url, path)
+        self.context_override.url = urljoin2(self.context_override.url, path)
         return node
 
-    def generic_visit(self, node, children):
-        if len(children) == 1:
-            return children[0]
+    def visit_header_mutation(self, node, children):
+        return self._visit_key_value_mutation(
+            node, children, self.context_override.headers)
 
-        new_children = []
-        for child in node:
-            if child.expr_name != '_':
-                new_children.append(child)
-        node.children = new_children
+    def visit_querystring_mutation(self, node, children):
+        return self._visit_key_value_mutation(
+            node, children, self.context_override.querystring_params)
+
+    def visit_body_mutation(self, node, children):
+        return self._visit_key_value_mutation(
+            node, children, self.context_override.body_params)
+
+    def _visit_key_value_mutation(self, node, children, dst_dict):
+        key = children[1].text
+        value = children[3].text
+        dst_dict[key] = value
+        return node
+
+    def visit_option_mutation(self, node, children):
+        if node.text not in self.context_override.options:
+            self.context_override.options.append(node.text)
+        return node
+
+    def visit_tool(self, node, children):
+        self.tool = node.text
+        return node
+
+    def visit_mutation(self, node, children):
+        self.context.update(self.context_override)
+        return node
+
+    def _final_context(self):
+        context = self.context.copy()
+        context.update(self.context_override)
+        return context
+
+    def visit_immutation(self, node, children):
+        context = self._final_context()
+
+        if children[0].expr_name == 'preview':
+            command = [self.tool]
+            if self.tool == 'httpie':
+                command += context.httpie_args(self.method)
+            else:
+                assert self.tool == 'curl'
+                command += context.curl_args(self.method)
+            click.echo(' '.join(command))
+        else:
+            assert children[0].expr_name == 'action'
+            httpie_main(context.httpie_args())
+
+    def generic_visit(self, node, children):
         return node
 
 
 def execute(command, context):
-    root = grammar.parse(command)
-    visitor = ExecutionVisitor(context)
-    visitor.visit(root)
+    try:
+        root = grammar.parse(command)
+    except ParseError:
+        click.echo('Invalid command')
+    else:
+        visitor = ExecutionVisitor(context)
+        visitor.visit(root)
