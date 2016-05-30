676ac3c5244d5e3a073f11f2550cb19d0590f2ce
Chang-Hung Liang <eliang.cs@gmail.com>
2016-05-30 17:05:22 +0800

    Use tox to test OS X

--- tests/utils.py
+++ tests/utils.py
@@ -9,14 +9,14 @@ def get_http_prompt_path():
     if sys.platform == 'win32':
         bin_name += '.exe'
 
-    bin_path = os.path.join(python_dir, bin_name)
-    if os.path.exists(bin_path):
-        return bin_path
-
-    # Try 'Scripts' directory (for Windows)
-    bin_path = os.path.join(python_dir, 'Scripts', bin_name)
-    if os.path.exists(bin_path):
-        return bin_path
+    paths = [
+        os.path.join(python_dir, bin_name),
+        os.path.join(python_dir, 'Scripts', bin_name),  # Windows
+        '/usr/bin/http-prompt'  # Homebrew installation
+    ]
+    for path in paths:
+        if os.path.exists(path):
+            return path
 
     raise OSError("could not locate http-prompt executable, "
                   "Python directory: %s" % python_dir)
