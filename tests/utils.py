039a7efd5e9f7bcabffb2f72cff96f68916a3920
Chang-Hung Liang <eliang.cs@gmail.com>
2016-05-30 11:54:19 +0800

    Use different console script path on Windows

--- tests/utils.py
+++ tests/utils.py
@@ -4,10 +4,10 @@ import sys
 
 def get_http_prompt_path():
     """Get the path to http-prompt executable."""
+    python_dir = os.path.dirname(sys.executable)
     bin_name = 'http-prompt'
     if sys.platform == 'win32':
-        bin_name += '.exe'
-    bin_path = os.path.join(os.path.dirname(sys.executable), bin_name)
-    if os.path.exists(bin_path):
-        return bin_path
-    return bin_name  # Try PATH
+        bin_path = os.path.join(python_dir, 'Scripts', bin_name + '.exe')
+    else:
+        bin_path = os.path.join(python_dir, bin_name)
+    return bin_path
