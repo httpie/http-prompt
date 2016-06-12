5d3b2cfa39a4389a34c2843db75f72de7db7fc26
Chang-Hung Liang <eliang.cs@gmail.com>
2016-06-12 15:41:33 +0800

    Fix #24: Make default style 'solarized'

--- http_prompt/defaultconfig.py
+++ http_prompt/defaultconfig.py
@@ -1,8 +1,8 @@
 # Highlighting style for prompt commands. Available values:
 # algol, algol_nu, autumn, borland, bw, colorful, default, emacs, friendly,
 # fruity, igor, lovelace, manni, monokai, murphy, native, paraiso-dark,
-# paraiso-light, pastie, perldoc, rrt, tango, trac, vim, vs, xcode
-command_style = 'monokai'
+# paraiso-light, pastie, perldoc, rrt, solarized, tango, trac, vim, vs, xcode
+command_style = 'solarized'
 
 # Highlighting style for HTTPie's output. Available values are the same as
 # command_style. Set this to None to use HTTPie's default setting.
