81f7fec594316be19d7e3aa63de48bebd8afe0bc
Timur Izhbulatov <timochka@gmail.com>
2021-02-16 10:34:05 +0300

    Use standard shlex.quote

--- http_prompt/utils.py
+++ http_prompt/utils.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 import math
 import re
+import shlex
 
 from prompt_toolkit.shortcuts import create_output
 
@@ -10,10 +11,7 @@ RE_ANSI_ESCAPE = re.compile(r'\x1b[^m]*m')
 
 
 def smart_quote(s):
-    # TODO: Escape
-    if ' ' in s or r'\:' in s:
-        s = "'" + s + "'"
-    return s
+    return shlex.quote(s)
 
 
 def unquote(s):
